161469e3645024d2c75a9e48536489e2e56db96a
Tomasz Kasperek <kasperekt@gmail.com>
2019-06-08 14:41:21 +0200

    Add config

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
 
-OUT_DIR = './out'
-VIS_DIR = './vis'
+on_remote = False
+if 'HOST_TYPE' in os.environ and os.environ['HOST_TYPE'] == 'remote':
+    print('Running on remote machine!')
+    on_remote = True
+
+
+OUT_DIR = '/artifacts' if on_remote else './out'
+VIS_DIR = '/artifacts/vis' if on_remote else './vis'
 
 
 def prepare_env():
