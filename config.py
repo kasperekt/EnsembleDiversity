035d4a9a22a671e84eefc8a3843ef061ca18a51c
Tomasz Kasperek <kasperekt@gmail.com>
2019-06-16 17:54:15 +0200

    Setup config

--- config.py
+++ config.py
@@ -1,17 +1,32 @@
 import os
 
 
-def get_results_dir():
-    key = 'HOST_TYPE'
+OUT_DIR = os.path.join(get_results_dir(), 'out')
+VIS_DIR = os.path.join(get_results_dir(), 'vis')
+DATA_DIR = get_storage_dir()
 
-    if key in os.environ and os.environ[key] == 'remote':
+
+def on_remote():
+    """
+    You should set `HOST_TYPE` env variable if on remote.
+    Look at the `scripts /` directory
+    """
+    key = 'HOST_TYPE'
+    return key in os.environ and os.environ[key] == 'remote'
+
+
+def get_results_dir():
+    if on_remote():
         return '/artifacts'
 
     return './'
 
 
-OUT_DIR = os.path.join(get_results_dir(), 'out')
-VIS_DIR = os.path.join(get_results_dir(), 'vis')
+def get_storage_dir():
+    if on_remote():
+        return '/storage/EnsembleDiversityData'
+
+    return '../EnsembleDiversityData'
 
 
 def prepare_env():
