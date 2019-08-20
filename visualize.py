3d03e54bc73f56d1eef27f885d00613b0ed7a067
Tomasz Kasperek <kasperekt@gmail.com>
2019-08-20 18:30:37 +0200

    Update plots

--- visualize.py
+++ visualize.py
@@ -10,7 +10,12 @@ from config import OUT_DIR, VIS_DIR, prepare_env
 
 def result_path(name: str, out_dir=OUT_DIR):
     filename = f'{name.lower()}experiment-ensemble.csv'
-    return os.path.join(out_dir, filename)
+    path = os.path.join(out_dir, filename)
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f'Data file ({path}) does not exist.')
+
+    return path
 
 
 def scatterplot(df, name, x, y='accuracy', datasets=['iris', 'cancer']):
@@ -39,28 +44,33 @@ def visualize():
 
     datasets = np.array(BINARY_CLASS_SETS)[:, 0]
     names = ['adaboost', 'catboost', 'lgb',
-             'randomforest', 'xgboost', 'bagging']
+             'randomforest', 'xgboost', 'bagging', 'all']
 
     for name in names:
-        df = pd.read_csv(result_path(
-            name, out_dir='../EnsembleDiversityResults/experiments-17-08'))
+        try:
+            csv_path = result_path(
+                name, out_dir='../EnsembleDiversityResults/experiments-10-08')
 
-        scatterplot(df, name + '__node-diversity',
-                    x='node_diversity', datasets=datasets)
-        scatterplot(df, name + '__used-attributes-ratio',
-                    x='used_attributes_ratio', datasets=datasets)
-        scatterplot(df, name + '__corr',
-                    x='corr', datasets=datasets)
-        scatterplot(df, name + '__q',
-                    x='q', datasets=datasets)
-        scatterplot(df, name + '__entropy',
-                    x='entropy', datasets=datasets)
-        scatterplot(df, name + '__kw',
-                    x='kw', datasets=datasets)
-        scatterplot(df, name + '__coverage-minmax',
-                    x='coverage_minmax', datasets=datasets)
-        scatterplot(df, name + '__coverage-std',
-                    x='coverage_std', datasets=datasets)
+            df = pd.read_csv(csv_path)
+
+            scatterplot(df, name + '__node-diversity',
+                        x='node_diversity', datasets=datasets)
+            scatterplot(df, name + '__used-attributes-ratio',
+                        x='used_attributes_ratio', datasets=datasets)
+            scatterplot(df, name + '__corr',
+                        x='corr', datasets=datasets)
+            scatterplot(df, name + '__q',
+                        x='q', datasets=datasets)
+            scatterplot(df, name + '__entropy',
+                        x='entropy', datasets=datasets)
+            scatterplot(df, name + '__kw',
+                        x='kw', datasets=datasets)
+            scatterplot(df, name + '__coverage-minmax',
+                        x='coverage_minmax', datasets=datasets)
+            scatterplot(df, name + '__coverage-std',
+                        x='coverage_std', datasets=datasets)
+        except FileNotFoundError as file_not_found_err:
+            print(file_not_found_err)
 
 
 if __name__ == '__main__':
